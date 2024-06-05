29de1f683ef38eb74178a748ed6a78eb3276a139
ZENISLEV\titoa <tito.ardha@windowslive.com>
2024-06-05 21:19:58 +0700

    removing vulnerable class

---
+++ Database/SQL Server/DatabaseSchema_Data/dbo/Stored Procedures/sp_db_compress.sql
@@ -0,0 +1,16 @@
+﻿-- =============================================
+-- Author	  : ArdhaGp
+-- Create date: 03-aug-2018
+-- Description:	Stored Procedure to compress database
+-- =============================================
+CREATE PROCEDURE sp_db_compress
+
+AS
+BEGIN
+    -- SET NOCOUNT ON added to prevent extra result sets from
+    -- interfering with SELECT statements.
+    SET NOCOUNT ON
+
+    -- Insert statements for procedure here
+ DBCC SHRINKFILE (2, TRUNCATEONLY)
+END
